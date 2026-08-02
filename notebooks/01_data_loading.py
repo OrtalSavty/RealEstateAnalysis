d6c6a09696120a085565ef6b4fdca84e4dd55267
Ortal <ortalsavty01@gmail.com>
2026-08-02 20:34:38 +0300

    הוספת עמודת עיר

--- notebooks/01_data_loading.py
+++ notebooks/01_data_loading.py
@@ -1,4 +1,4 @@
-# עבודה עם טבלאות נתונים 
+# עבודה עם טבלאות נתונים
 import pandas as pd
 import numpy as np
 # איתור וקריאת קבצים מרובים מתיקיות המחשב
@@ -16,6 +16,7 @@ all_files = glob.glob(os.path.join(path,"*.csv"))
 # ניייצר רשימה ריקה שבתוכה נשמור את כל הטבלאות לפני שנאחד אותם
 df_list = []
 
+
 # מעבר על כל הנתיבים
 for file in all_files:
     # נחריז שהקוד הבא עלול לזרוק שגיע אם פייתון לא תצליח לקרוא את הנתונים בצורה תקינה    
@@ -25,6 +26,24 @@ for file in all_files:
         # file, encoding_errors='ignore' אומר לפייתון להתעלם מתווים בעיתיים
         # encoding='utf-8' אומר לפייתון שהקובץ יכיל נתונים בעברית אז שלא יבלגן אותם
         df = pd.read_csv(file, encoding_errors='ignore', encoding='utf-8')
+            
+               #נוציא את שם הקובץ בלי השם הנתיב        
+        file_name = os.path.basename(file)
+        # נחתוך את ההתחלה הקבועה        
+        city_name_not_final1 = file_name.replace("deals_no_address_", "")
+         # נחחליף את כל המקף תחתון ברווחים        
+        city_name_not_final2 = city_name_not_final1.replace("_", " ")
+    # נהפוך את המחרוזת לרשימה של מילים        
+        words_list = city_name_not_final2.split()
+         
+    # נעבור על כל המילים במחרוזת       
+        for word in words_list:
+        # ניקח את הכל מלבד המילה האחרונה            
+            city_name = " ".join(words_list[:-1])
+            
+        # הוספת העיר לקובץ       
+        df['city'] = city_name
+        
         # הוספה של הקובץ למשתנה הטבלאות שלנו        
         df_list.append(df)
         
@@ -36,9 +55,10 @@ for file in all_files:
 # ignore_index=True הגדרנו שיתעלם מהאינדקסים בטבלה ויעשה אינדקסים חדשים שהטבלה תיהיה מסודרת
 all_data = pd.concat(df_list, ignore_index=True)
 
-# נסגר את השמות לאנגלית
+# נסדר את השמות לאנגלית
 # מתן שמות חדשים ונקיים באנגלית לעמודות
-all_data.columns = ['id','address','area','date', 'price','Block and parcel','neighborhood','rooms','floor', 'A trend of change']
+all_data.columns = ['id','address','area','date', 'price','Block and parcel','neighborhood','rooms','floor', 'A trend of change', 'city']
+
 
 # ניקוי עמודת המחיר
 # הסרת סימני מטבע (₪), פסיקים או רווחים והמרה למספר
